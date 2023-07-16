64cce620c405f7d624d4200919b65c3557a79456
Zulfiqar Ali <11179904+zulfiqarpk92@users.noreply.github.com>
2023-07-16 11:28:55 +0500

    add more images to the blogs factory

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -13,11 +13,17 @@ class BlogFactory extends Factory
 
   public function definition()
   {
+    $images = [
+        'images/DU4hLjGPEMwAgvZ28i3NhUDLDeTOX0i9AVDSMp8e.jpg',
+        'images/jee5TLQFITpZkCUsKMLGnkHRo9B2HrNzLnxDXGSu.jpg',
+        'images/qSkzjhVn5cnpovdOElvJP9DGKA2SjwROkdDXb3rc.jpg',
+        'images/wEEPWzrqqQCgrBCaiEbz0dzBuhWPwrV837JOJrXj.jpg'
+    ];
     return [
       'id'            => Blog::max('id') + 1,
       'title'         => $this->faker->sentence,
       'body'          => $this->faker->paragraphs(3, true),
-      'image_path'    => 'images/442SscGn4UYOdv6unOtXM8q2iFLbpYTk2hoaQSlc.jpg',
+      'image_path'    => $images[array_rand($images, 1)],
       'user_id'       => User::pluck('id')->random(),
       'published_at'  => Carbon::now()->subDays(rand(0, 30)),
     ];
